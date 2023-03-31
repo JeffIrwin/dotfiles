cde48283932886fef712f7f48f4c5f473dc72208
Jeff Irwin <jirwin505@gmail.com>
2023-03-31 19:34:23 -0400

    comments and tweaks

--- install.sh
+++ install.sh
@@ -11,12 +11,18 @@ cp .tmux.conf ~
 ## TODO: modify this file and copy it if you don't already have one
 #cp .tmux.conf.local.example ~/.tmux.conf.local
 
+# This works with the repo cloned anywhere.  If you're going to modify and push
+# changes, it's better to actually clone directly to the destination
+#
+# We don't really need the tmux files here but 🤷
 mkdir -p ~/.config/nvim/
 cp -r ./* ~/.config/nvim/
 
+# Setup packer as the nvim package manager
 rm -rf ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 #nvim +PackerSync +qall
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+nvim -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 
