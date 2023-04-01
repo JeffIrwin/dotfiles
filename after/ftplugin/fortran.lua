74a39703d303316f185364983f4bb0ca6653c8ad
Jeff Irwin <jirwin505@gmail.com>
2023-04-01 19:13:37 -0400

    set fortran textwidth to 80

--- after/ftplugin/fortran.lua
+++ after/ftplugin/fortran.lua
@@ -2,3 +2,7 @@
 -- omfg it was impossible to figure out that i needed to put this here
 vim.o.expandtab = false
 
+-- TODO: is it possible to put these in a global after/init.lua file?  I like
+-- these as sane defaults for all filetypes, not just Fortran
+vim.o.textwidth = 80
+
