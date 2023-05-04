d3090b39c892032c30b4112a5ff27047f7635827
Jeff Irwin <jeff.irwin@autodesk.com>
2023-05-04 19:06:39 -0400

    use different settings for free vs fixed form fortran

--- after/ftplugin/fortran.lua
+++ after/ftplugin/fortran.lua
@@ -1,8 +1,30 @@
 
--- omfg it was impossible to figure out that i needed to put this here
-vim.o.expandtab = false
+--print("hello from after/ftplugin/fortran.lua")
 
--- TODO: is it possible to put these in a global after/init.lua file?  I like
--- these as sane defaults for all filetypes, not just Fortran
-vim.o.textwidth = 80
+-- For more advanced free-form vs fixed-form detection, see:
+--
+--     /mnt/c/Program Files (x86)/Vim/vim80/ftplugin/fortran.vim
+--
+-- However, this fast simple method should work for me
+
+local ext = vim.fn.expand("%:e")
+if ext == "f90" or ext == "F90" then
+	--print("extension is  f90/F90")
+
+	--vim.o.tabstop    = 4
+	--vim.o.shiftwidth = 4
+	--vim.o.textwidth = 80
+
+	-- omfg it was impossible to figure out that i needed to put this here
+	vim.o.expandtab = false
+
+	-- TODO: is it possible to put these in a global after/init.lua file?  I like
+	-- these as sane defaults for all filetypes, not just Fortran
+	vim.o.textwidth = 80
+
+else
+	--print("extension not f90")
+	--
+	-- Keep defaults, e.g. textwidth = 72
+end
 
--- after/plugin/init.lua
+++ after/plugin/init.lua
@@ -3,14 +3,32 @@
 -- defaults
 
 --print("hello from after")
+--
+--print("vim.b.fortran_fixed_source = ", vim.b.fortran_fixed_source)
+--print("vim.b.fortran_free_source = ", vim.b.fortran_free_source)
+--print("vim.b = ", vim.b)
+--print("")
+----print("vim.o.fortran_fixed_source = ", vim.o.fortran_fixed_source)
+----print("vim.o.fortran_free_source = ", vim.o.fortran_free_source)
+----print("vim.o.fortran_have_tabs = ", vim.o.fortran_have_tabs)
+--
+--print("expand... = ", vim.fn.expand("%:e"))
+
+--local ext = vim.fn.expand("%:e")
+--if ext == "f90" or ext == "F90" then
+--	--print("extension is  f90/F90")
 
 vim.o.tabstop    = 4
 vim.o.shiftwidth = 4
-
 vim.o.textwidth = 80
 
--- nvim thinks tabs are illegal in Fortran.  They're not
-vim.cmd("hi link fortranTab NONE")
+--else
+--	--print("extension not f90")
+--end
+
+---- nvim thinks tabs are illegal in Fortran.  They're not
+--vim.cmd("hi link fortranTab NONE")
+--vim.o.fortran_have_tabs=1
 
 -- Display leading spaces as dark grey (plays well with default colorscheme and
 -- koehler).  Note that this has to be applied *after* setting the colorscheme
@@ -23,5 +41,13 @@ vim.cmd("hi link fortranTab NONE")
 --vim.cmd("highlight WhiteSpaceBol ctermbg=lightgreen")
 --vim.cmd("highlight WhiteSpaceBol ctermbg=darkgrey")
 vim.cmd("highlight WhiteSpaceBol guibg=darkgrey")
-vim.cmd('match WhiteSpaceBol /^ \\+/')
+--vim.cmd('match WhiteSpaceBol /^ \\+/')
+
+--vim.cmd.autocmd("FileType netrw nmap <buffer> <C-j> <C-w><C-j>")
+
+---- To work in multiple splits within a window, it has to be an autocmd
+
+--vim.api.nvim_create_augroup("custom_highlighting", {
+	vim.cmd.autocmd("VimEnter,WinEnter * match WhiteSpaceBol /^ \\+/")
+--})
 
