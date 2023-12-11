62a993a42efdbe056007dbdf3a1da046d59f5499
Jeff Irwin <jirwin505@gmail.com>
2023-12-10 22:12:16 -0500

    treat syntran as rust files and set sane rust defaults

---
+++ after/ftplugin/rust.lua
@@ -0,0 +1,7 @@
+
+vim.o.expandtab = false
+
+-- TODO: is it possible to put these in a global after/init.lua file?  I like
+-- these as sane defaults for all filetypes, not just Fortran
+vim.o.textwidth = 80
+
---
+++ ftdetect/syntran.lua
@@ -0,0 +1,4 @@
+
+-- Highlight syntran files as if they were rust
+vim.cmd.autocmd("BufRead,BufNewFile *.syntran set filetype=rust")
+
