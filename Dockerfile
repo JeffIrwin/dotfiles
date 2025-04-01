f2f8b2ca21baf5bcef719453efc011c4383cc63c
Jeff Irwin <jirwin505@gmail.com>
2025-04-01 19:53:36 -0400

    remove more extraneous things

--- Dockerfile
+++ Dockerfile
@@ -22,14 +22,7 @@ ENV PATH=$PATH:/opt/nvim-linux-x86_64/bin/
 # pip can't run as root without "breaking" system packages
 #RUN pip install fortls  --break-system-packages   # install fortran lsp
 RUN pip install fortls  # install fortran lsp
-RUN pip install pyright # install python lsp
 #RUN fortls --version
-
-# Install lua lsp
-RUN curl -LO https://github.com/LuaLS/lua-language-server/releases/download/3.13.9/lua-language-server-3.13.9-linux-x64.tar.gz
-RUN tar xvf lua-language-server-3.13.9-linux-x64.tar.gz
-RUN ln -s $PWD/bin/lua-language-server /usr/local/bin/lua-language-server
-RUN lua-language-server --version
 
 COPY . .
 RUN ./install.sh
--- init.lua
+++ init.lua
@@ -32,34 +32,9 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	spec = {
 		-- add your plugins here
-
 		{
 			"neovim/nvim-lspconfig"
 		},
-
-		{
-			"folke/tokyonight.nvim"
-		},
-
-		{
-			"christoomey/vim-tmux-navigator",
-			cmd = {
-				"TmuxNavigateLeft",
-				"TmuxNavigateDown",
-				"TmuxNavigateUp",
-				"TmuxNavigateRight",
-				"TmuxNavigatePrevious",
-				"TmuxNavigatorProcessList",
-			},
-			keys = {
-				{ "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
-				{ "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
-				{ "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
-				{ "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
-				{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-			},
-		},
-
 		{
 			-- Source for configuring nvim-lint with gfortran:
 			--
@@ -70,26 +45,23 @@ require("lazy").setup({
 			config = function()
 				local lint = require "lint"
 				local gfortran_diagnostic_args =
-				-- TODO: how to get include dir for .mod files? maybe fpm install lib
-				-- true, then include -I~/.local/include ?
 				{
+					-- Add your module location after `-I`, otherwise linting
+					-- will stop after the first unfound `use`
+
+					"-I./build/include/",
 					"-Wall",
 					"-Wextra",
 					"-Wno-tabs",
-					--"-I./build/gfortran_2654F75F5833692A/",
-					"-I./build/include/",
 					"-fmax-errors=5",
 				}
-
 				lint.linters_by_ft = {
 					fortran = {
 						"gfortran",
 					},
 				}
-
 				local pattern = "^([^:]+):(%d+):(%d+):%s+([^:]+):%s+(.*)$"
 				local groups = { "file", "lnum", "col", "severity", "message" }
-
 				local severity_map = {
 					["Error"] = vim.diagnostic.severity.ERROR,
 					["Warning"] = vim.diagnostic.severity.WARN,
@@ -110,7 +82,6 @@ require("lazy").setup({
 					parser = require("lint.parser").from_pattern(pattern, groups, severity_map, defaults),
 				}
 			end,
-
 		}
 	},
 
@@ -120,11 +91,7 @@ require("lazy").setup({
 	--install = { colorscheme = { "tokyonight-moon" } },
 
 	-- automatically check for plugin updates
-	checker =
-	{
-		enabled = true,
-		notify = false  -- don't nag
-	},
+	checker = { enabled = true, },
 })
 --------------------------------------------------------------------------------
 
@@ -138,99 +105,6 @@ vim.api.nvim_create_autocmd({ "BufEnter", "InsertLeave", "BufWritePost" }, {
 })
 
 --------------------------------------------------------------------------------
-
----- Builtin colorschemes, no packer required
---vim.cmd.colorscheme("default")
---vim.cmd.colorscheme("koehler")
-
--- These colorschemes require packer plugins
-
-----vim.opt.background = "dark" -- or "light" for light mode
-----vim.cmd.colorscheme("gruvbox")
-
---vim.cmd.colorscheme("tokyonight-night")
---vim.cmd.colorscheme("tokyonight-storm")
-vim.cmd.colorscheme("tokyonight-moon")
---vim.cmd.colorscheme("habamax")
-
-----vim.cmd.colorscheme("iceberg")
---vim.cmd.colorscheme("rose-pine")
-
---********
-
--- Hybrid line numbers: show the current line as absolute and others as relative
-vim.opt.number         = true
-vim.opt.relativenumber = true
-
--- Remap split-window navigation commands, e.g. Ctrl+j instead of the standard
--- Ctrl+w Ctrl+j
---
--- Ref:
---
---     https://thoughtbot.com/blog/vim-splits-move-faster-and-more-naturally#easier-split-navigations
---
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { noremap = true })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { noremap = true })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { noremap = true })
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { noremap = true })
-
--- idk why i have to set these TmuxNavigate* bindings in two places
-vim.keymap.set("n", "<c-h>", "<cmd>TmuxNavigateLeft<cr>", { noremap = true })
-vim.keymap.set("n", "<c-j>", "<cmd>TmuxNavigateDown<cr>", { noremap = true })
-vim.keymap.set("n", "<c-k>", "<cmd>TmuxNavigateUp<cr>", { noremap = true })
-vim.keymap.set("n", "<c-l>", "<cmd>TmuxNavigateRight<cr>", { noremap = true })
-vim.keymap.set("n", "<c-\\>", "<cmd>TmuxNavigatePrevious<cr>", { noremap = true })
-
--- netrw (explorer) overrides normal keymaps.  this breaks the tmux integration
--- and i'm not sure if i can do anything about that (although it works if you
--- edit a normal file from netrw)
-vim.cmd.autocmd("FileType netrw nmap <buffer> <C-j> <C-w><C-j>")
-vim.cmd.autocmd("FileType netrw nmap <buffer> <C-k> <C-w><C-k>")
-vim.cmd.autocmd("FileType netrw nmap <buffer> <C-l> <C-w><C-l>")
-vim.cmd.autocmd("FileType netrw nmap <buffer> <C-h> <C-w><C-h>")
-
--- Open new splits to the right and bottom
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
--- Use standard vim yank `y` and put `p` to copy/paste into/out-of vim
-vim.api.nvim_set_option_value("clipboard", "unnamed", {})
-
----- not sure why i resorted to the stuff below, but the "clipboard" "unnamed"
----- setting above, in conjunction with "save_to_clipboard" in alacritty.toml,
----- has copy and paste working in all contexts
-
---vim.g.clipboard =
---{
---	name = 'WslClipboard',
---	copy = {
---		["+"] = 'clip.exe',
---		["*"] = 'clip.exe',
---	 },
---	paste = {
---		["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
---		["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
---	},
---	cache_enabled = 0,
---}
-
---------------------------------------------------------------------------------
-
--- The leader key is like your personal namespace for vim commands.  Any command
--- defined with the leader as a prefix is guaranteed not to clash with any
--- builtin keybinding.
-vim.g.mapleader = " "
-
---vim.keymap.set("n", "<leader>a", "<C-^>"     , {noremap = true}) -- alternate to the prev opened buf
-vim.keymap.set("n", "<leader> ", "<C-^>", { noremap = true }) -- alternate to the prev opened buf
-
-vim.keymap.set("n", "<leader>f", ":find ", { noremap = true })
-vim.keymap.set("n", "<leader>l", ":ls<CR>:b ", { noremap = true })
-vim.keymap.set("n", "<leader>n", ":bnext<CR>", { noremap = true })
-vim.keymap.set("n", "<leader>p", ":bprev<CR>", { noremap = true })
-vim.keymap.set("n", "<leader>e", ":Ex<CR>", { noremap = true })
-vim.keymap.set("n", "<leader>s", ":Sex<CR>", { noremap = true })
-vim.keymap.set("n", "<leader>v", ":Vex!<CR>", { noremap = true })
 
 -- Create the lsp keymaps only when a language server is active
 vim.api.nvim_create_autocmd('LspAttach', {
@@ -253,100 +127,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
 --------------------------------------------------------------------------------
 
--- Jeff's dumb supertab replacement
-vim.keymap.set("i", "<Tab>",
-	function()
-		local col = vim.fn.col(".") - 1
-
-		-- If we're at the beginning of the line or on whitespace, insert a tab
-		-- literal.  Otherwise, map to previous autocomplete (Ctrl+p)
-		--
-		-- Ref:
-		--
-		--     https://github.com/elianiva/dotfiles/blob/b0742981158c89063593ce27f74f780f3474d331/nvim/.config/nvim/lua/modules/_util.lua
-
-		if col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
-			return "<Tab>"
-		else
-			return "<C-p>"
-		end
-	end, { expr = true, noremap = true })
-
---------------------------------------------------------------------------------
-
--- changemewtf's fuzzy finder.  Usage: ```<leader>f *pattern*<Tab>``` or
--- ```:find *pattern*<Enter>```.  For example, I have a file under this
--- directory with this path and name:
---
---     ./after/ftplugin/fortran.lua
---
--- To edit it, simply type (in normal mode) " f for<Tab><Enter>".  That's
--- assuming there's no other match before it in the tab completion menu.
-vim.opt.path:append { "**" } -- you *must* use ".opt" here, not just ".o"
-
--- TODO: try making a project-specific include which appends things like 3p
-vim.opt.wildignore:append { "*/scratch/*", "*/target/*", "*/build/*" }
-
---------------------------------------------------------------------------------
-
--- LSPs
-
----- Install:  npm i -g bash-language-server
---require("lspconfig").bashls.setup{}
---vim.api.nvim_create_autocmd('FileType', {
---  pattern = 'sh',
---  callback = function()
---    vim.lsp.start({
---      name = 'bash-language-server',
---      cmd = { 'bash-language-server', 'start' },
---    })
---  end,
---})
-
---********
---lua
-
-require("lspconfig").lua_ls.setup {
-	on_init = function(client)
-		if client.workspace_folders then
-			local path = client.workspace_folders[1].name
-
-			if path ~= vim.fn.stdpath('config') and (vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc')) then
-				return
-			end
-		end
-
-		client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-			runtime = {
-				-- Tell the language server which version of Lua you're using
-				-- (most likely LuaJIT in the case of Neovim)
-				version = 'LuaJIT'
-			},
-			-- Make the server aware of Neovim runtime files
-			workspace = {
-				checkThirdParty = false,
-				library = {
-					vim.env.VIMRUNTIME,
-					--vim.env.VIMRUNTIME + "/lua",
-					-- Depending on the usage, you might want to add additional paths here.
-					--"${3rd}/luv/library",
-					-- "${3rd}/busted/library",
-				}
-				-- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
-				-- library = vim.api.nvim_get_runtime_file("", true)
-			}
-		})
-	end,
-	settings = {
-		Lua = {}
-	}
-}
-
---********
--- fortran
-
--- fortls only seems to work in git repos. or maybe it needs fpm.toml or
--- something :shrug:
+-- fortran lsp
 require("lspconfig").fortls.setup {
 	cmd = {
 		'fortls',
@@ -357,12 +138,6 @@ require("lspconfig").fortls.setup {
 	}
 }
 
---********
--- python
-require("lspconfig").pyright.setup {}
-
---********
-
 -- fortran linting
 vim.api.nvim_create_autocmd({ "BufEnter", "InsertLeave", "BufWritePost" }, {
 	callback = function()
@@ -372,8 +147,3 @@ vim.api.nvim_create_autocmd({ "BufEnter", "InsertLeave", "BufWritePost" }, {
 		end
 	end
 })
-
---------------------------------------------------------------------------------
-
-vim.cmd("hi! Normal ctermbg=NONE guibg=NONE")
-
